f26ffdc7c4cf82c8eb75cd1d1ae3c50c8329f3a6
Nate Hilliard <phaionix@users.noreply.github.com>
2016-04-24 20:45:08 -0500

    Added plot subhalos function.

--- local_working.py
+++ local_working.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 import os
 import illustris_python as il
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+def plot_subhalo_pts(subhalos):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    xs = [xyz[0] for xyz in subhalos['SubhaloPos']]
+    ys = [xyz[1] for xyz in subhalos['SubhaloPos']]
+    zs = [xyz[2] for xyz in subhalos['SubhaloPos']]
+    ax.scatter(xs, ys, zs)
+    plt.show()
 
 if __name__ == '__main__':
     #add "export ILLUSTRISAPIKEY='insert your api key here'"
@@ -14,4 +26,10 @@ if __name__ == '__main__':
     fields = ['SubhaloMass', 'SubhaloPos', 'SubhaloVel']
     subhalos = il.groupcat.loadSubhalos(basePath,snapNum,fields=fields)
 
-    print(subhalos['count'])
+    H, edges = np.histogramdd(subhalos['SubhaloPos'], bins=(50,50,50),\
+                              weights=subhalos['SubhaloMass'])
+
+    plot_subhalo_pts(subhalos)
+
+    #print(subhalos['SubhaloPos'].shape)
+    
